37e0ddf0b8a3c63caaf20a4f341bd311d1a55b9a
Mohammadshadathusen <shadatrolli05@gmail.com>
2025-11-23 04:39:26 +0530

    Adding for web services

--- app.py
+++ app.py
@@ -38,7 +38,9 @@ def predict():
     output=model.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House Price Prediction Value is{}".format(output))
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
 
 
